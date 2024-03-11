4748e271266f8e0f3867d01cb85a3096f64ba5ed
Joel Wanjohi <joelwanjohi207@gmail.com>
2024-03-11 22:04:55 +0300

    klm

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ let myprogressbar = document.getElementById('myprogressbar');
 let previous = document.getElementById('previous');
 let next = document.getElementById('next');
 let mastersongname = document.getElementById('mastersongname')
+
+let songs = [
+    {songName : "Wishing Well", filePath:"audio/1.mp3", coverPath:"logos/cover.mp3"},
+    {songName : "Promises", filePath:"audio/2.mp3", coverPath:"logos/cover1.mp3"},
+    {songName : "Paint City Red", filePath:"audio/3.mp3", coverPath:"/logos/cover2.mp3"},
+    {songName : "Hotline Blink", filePath:"audio/4.mp3", coverPath:"logos/cover3.mp3"},
+    {songName : "Caddy Shop", filePath:"audio/5.mp3", coverPath:"logos/cover.mp3"},
+]
