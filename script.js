25a90ffb3fbce75924b91ab3144df150f158e3b9
Joel Wanjohi <joelwanjohi207@gmail.com>
2024-03-11 22:00:50 +0300

    added javascript

---
+++ script.js
@@ -0,0 +1,8 @@
+// #initialize the variable
+let songIndex = 1;
+let audioElement = new Audio('audio/1.mp3');
+let masterplay = document.getElementById('masterplay');
+let myprogressbar = document.getElementById('myprogressbar');
+let previous = document.getElementById('previous');
+let next = document.getElementById('next');
+let mastersongname = document.getElementById('mastersongname')
